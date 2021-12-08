d033c61763d3c8818970a86aed5a88b59727b658
AliaksandrRauko <ravkoaal@mail.ru>
2021-12-08 11:56:52 +0300

    Setup calculations

--- HomeWork_2_1_2/ViewController.swift
+++ HomeWork_2_1_2/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
+enum Colour {
+    case green
+    case yellow
+    case red
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var redView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    private var signColour = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        greenView.alpha = 0.3
+        yellowView.alpha = 0.3
+        redView.alpha = 0.3
+        signColour = "green"
     }
 
-
+    @IBAction func startPressed(_ sender: UIButton) {
+        
+        var secondStart = false
+        
+        if !secondStart {
+            startButton.setTitle("Next", for: .normal)
+        }
+        
+        switch signColour {
+        case "green":
+            greenView.alpha = 1
+            yellowView.alpha = 0.3
+            redView.alpha = 0.3
+            signColour = "yellow"
+            secondStart = true
+        case "yellow":
+            greenView.alpha = 0.3
+            yellowView.alpha = 1
+            redView.alpha = 0.3
+            signColour = "red"
+        case "red":
+            greenView.alpha = 0.3
+            yellowView.alpha = 0.3
+            redView.alpha = 1
+            signColour = "green"
+        default:
+            break
+        }
+    }
 }
 
